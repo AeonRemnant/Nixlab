0b78a99e8743bd27c237fbf22480944a3a14cb90
AeonRemnant <aeonremnant@github.com>
2025-04-21 11:17:54 +1000

    Removed old Comin config.

--- hosts/coredns/configuration.nix
+++ hosts/coredns/configuration.nix
@@ -23,13 +23,6 @@
   networking.firewall.allowedUDPPorts = [ 53 ];
   networking.firewall.allowedTCPPorts = [ 53 ];
 
-  services.comin = {
-    enable = true;
-    repositoryUrl = "https://github.com:aeonremnant/nixlab.git";
-    flakeUri = ".#coredns";
-    schedule = "*:0/01";
-  };
-
   # System state version
   system.stateVersion = "unstable";
 }
